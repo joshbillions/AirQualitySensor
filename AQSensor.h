91ebc02d618a6179b3b0b6dc6b7620d941da8084
Josh Billions <josh@mblabs.org>
2021-11-23 14:57:24 -0600

    Initial commit. Working.

---
+++ AQSensor.h
@@ -0,0 +1,80 @@
+#include "Adafruit_PM25AQI.h"
+#include <FastLED.h>
+
+struct AirQualitySensor : Service::AirQualitySensor {
+  CRGB *statusLED;
+  //Adafruit_PM25AQI *sensor;
+  Adafruit_PM25AQI sensor;
+  SpanCharacteristic *airQuality;
+  SpanCharacteristic *pm10Density;
+  SpanCharacteristic *pm25Density;
+
+  AirQualitySensor(CRGB &led) : Service::AirQualitySensor() {
+    statusLED = &led;
+    sensor = Adafruit_PM25AQI();
+    airQuality = new Characteristic::AirQuality(0);
+    pm10Density = new Characteristic::PM10Density(0.0);
+    pm25Density = new Characteristic::PM25Density(0.0);
+
+    Serial.print("Configuring Air Quality Sensor");
+    Serial.print("\n");
+
+    if (!sensor.begin_I2C()) {
+      Serial.println("Could not find PM 2.5 sensor!");
+      while(1) delay(10);
+    } else {
+      Serial.println("Found PM 2.5 Sensor");
+    }
+  }
+
+  void loop() {
+    if (airQuality->timeVal() < 5000) {
+      return;
+    }
+
+    PM25_AQI_Data data;
+
+    if (!sensor.read(&data)) {
+      statusLED->setRGB(255, 255, 255);
+      Serial.println("Failed to read sensor");
+      return;
+    }
+
+    Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
+    Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
+    Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
+
+    double pm100 = double(data.particles_100um);
+    double pm25 = double(data.particles_25um);
+
+    int homeKitQuality = 0;
+
+    if (pm25 <= 12) {
+      homeKitQuality = 1; // Excellent
+      statusLED->setRGB(0, 255, 0); // Green
+      Serial.println("Excellent");
+    } else if (pm25 <= 35) {
+      homeKitQuality = 2; // Good
+      statusLED->setRGB(154, 205, 50); // YellowGreen
+      Serial.println("Good");
+    } else if (pm25 <= 55) {
+      homeKitQuality = 3; // Fair
+      statusLED->setRGB(255, 255, 0); // Yellow
+      Serial.println("Fair");
+    } else if (pm25 <= 150) {
+      homeKitQuality = 4; // Inferior
+      statusLED->setRGB(255, 0, 0); // Red
+      Serial.println("Inferior");
+    } else {
+      homeKitQuality = 5; // Poor
+      statusLED->setRGB(128, 0, 128); // Purple
+      Serial.println("Poor");
+    }
+
+    airQuality->setVal(homeKitQuality);
+    pm10Density->setVal(pm100);
+    pm25Density->setVal(pm25);
+
+  }
+
+};
---
+++ AirQualitySensor.ino
@@ -0,0 +1,54 @@
+// HomeKit
+#include "HomeSpan.h"
+#include "AQSensor.h"
+#include <FastLED.h>
+
+// Thing Plus C Status Indicator
+#define LED_PIN     2
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2812
+#define NUM_LEDS    1
+#define BRIGHTNESS  128
+
+// Watchdog
+#include <esp_task_wdt.h>
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+
+  // Wait for sensor to come up
+  delay(2000);
+
+  //esp_task_wdt_init(3, true);
+  //esp_task_wdt_add(NULL);
+
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( BRIGHTNESS );
+  FastLED.clear();
+  FastLED.show();
+
+  homeSpan.enableOTA();
+  homeSpan.begin(Category::Sensors, "Air Quality Sensor");
+  new SpanAccessory();
+  new Service::AccessoryInformation();
+  new Characteristic::Name("Air Quality Sensor");
+  new Characteristic::Manufacturer("Josh Billions");
+  new Characteristic::SerialNumber("123-ABC");
+  new Characteristic::Model("Air Quality Sensor");
+  new Characteristic::FirmwareRevision("0.91");
+  new Characteristic::Identify();
+
+  new Service::HAPProtocolInformation();
+  new Characteristic::Version("1.1.0");
+  
+  new AirQualitySensor(leds[0]); 
+}
+
+void loop() {
+  esp_task_wdt_reset();
+  homeSpan.poll();
+  FastLED.show();
+}
